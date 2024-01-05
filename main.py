a5c13fd43881eaca1e4eb42dd36efea714512f3b
19SHart <155738159+19SHart@users.noreply.github.com>
2024-01-05 12:26:30 +0000

    Start Ask Function

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+print("Welcome to the fun fair!")
 
+ChildPrice = 12
+AdultPrice = 20
+SeniorPrice = 11
+WristbandPrice = 20
+ParkingPrice = 0
+
+def ticketAsk(ticket, pretty):
+    while True: 
+        try:
+            user = int(input("How many {pretty} would you like? "))
+        except ValueError:
+
+print("Current Prices:\nChild: {ChildPrice}\nAdult: {AdultPrice}\nSenior: {SeniorPrice}\nWristband: {WristbandPrice}\nParking: {ParkingPrice}")
